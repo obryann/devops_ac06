ab342db18f3811f91508c92de4466381c7e4f069
Guilherme Domingues Reinhardt <1901228@SALAS.AULAS>
2019-10-03 21:54:03 -0300

    adicionando comentários

--- com/ac5/converte_hora.py
+++ com/ac5/converte_hora.py
@@ -1,12 +1,16 @@
+"""Função"""
+
+
 def converteHora(hora24, minuto24):
+    """Verifica se os valores são válidos"""
     if (hora24 > 23) or (hora24 < 0) or (minuto24 < 0) or (minuto24 > 59):
         return None
-
+    """Verifica se a hora é igual a zero"""
     if (hora24 < 12):
         if (hora24 == 0):
             hora24 = 12
         return '%02d:%02d AM' % (hora24, minuto24)
-
+    """Verifica se a hora é maior que 12"""
     if (hora24 > 12):
         hora24 -= 12
     return '%02d:%02d PM' % (hora24, minuto24)
--- com/ac5/test/test_converte_hora.py
+++ com/ac5/test/test_converte_hora.py
@@ -1,14 +1,33 @@
-import pytest 
+import pytest
 from com.ac5.converte_hora import converteHora
 
+"""Faz o teste de horário nulo"""
+
+
 def test_hora():
-    assert converteHora(24, 60) == None, 'Should be None' 
+    """Faz o teste com 24 horas e 60 minutos"""
+    assert converteHora(24, 60) is None, 'Should be None'
+
+
+"""Faz o segundo teste, teste de horário manhã"""
+
 
 def test_hora2():
+    """Faz o teste com 5 horas e 10 minutos"""
     assert converteHora(5, 10) == '05:10 AM'
 
+
+"""Faz o terceiro teste, teste de horário da tarde"""
+
+
 def test_hora3():
+    """Faz o teste com 13 horas e 10 minutos"""
     assert converteHora(13, 10) == '01:10 PM'
 
+
+"""faz o quarto teste, teste de horário da madrugada"""
+
+
 def test_hora4():
+    """Faz o teste com 0 horas e 10 minutos"""
     assert converteHora(0, 0) == '12:00 AM'
