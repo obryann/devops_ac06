8d31ec8ab7d51fcf6d3b60a96b78dd60319cf309
obryann <1901236@SALAS.AULAS>
2019-10-03 22:24:54 -0300

    aaaaaaaaaa

--- com/ac5/test/test_calcula_parcela.py
+++ com/ac5/test/test_calcula_parcela.py
@@ -1,6 +1,5 @@
-from com.ac5.calcula_parcela import valor_pagamento
-
 """teste de valor de pagamento"""
+from com.ac5.calcula_parcela import valor_pagamento
 
 
 def test_valor_pagamento1():
@@ -8,15 +7,9 @@ def test_valor_pagamento1():
     assert valor_pagamento(-1, 20) == 0, "Should be 0"
 
 
-"""teste de valor de pagamento"""
-
-
 def test_valor_pagamento2():
     """ verifica se é 90.4"""
     assert valor_pagamento(80, 10) == 90.4, "Should be 90,4"
-
-
-"""teste de valor de pagamento"""
 
 
 def test_valor_pagamento3():
