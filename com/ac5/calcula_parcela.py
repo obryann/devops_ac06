11062fa2f7435e8ba18e56b219f8f1342d6bf9a7
Vitmyself <vitor.duarte@aluno.faculdadeimpacta.com.br>
2019-10-03 21:53:11 -0300

    dando espaco e retirando paranteses

--- com/ac5/calcula_parcela.py
+++ com/ac5/calcula_parcela.py
@@ -3,11 +3,11 @@
 
 def valor_pagamento(valor, dias_atraso):
     """ verifica o valor de pagamento conforme os dias de atraso """
-    if (valor < 0):
+    if valor < 0:
         return None
-    if (dias_atraso > 0):
+    if dias_atraso > 0:
         multa = valor * 0.03
         adicional_atraso = valor * (dias_atraso * 0.01)
         return valor + multa + adicional_atraso
-    if (dias_atraso <= 0):
+    if dias_atraso <= 0:
         return valor
--- com/ac5/test/test_calcula_parcela.py
+++ com/ac5/test/test_calcula_parcela.py
@@ -1,15 +1,21 @@
 import pytest
 from com.ac5.calcula_parcela import valor_pagamento
 
+"""teste de valor de pagamento"""
+
 
 def test_valor_pagamento1():
     """verifica se é vazio"""
     assert valor_pagamento(-1, 20) is None, "Should be None"
 
+"""teste de valor de pagamento"""
+
 
 def test_valor_pagamento2():
     """ verifica se é 90.4"""
     assert valor_pagamento(80, 10) == 90.4, "Should be 90,4"
+
+"""teste de valor de pagamento"""
 
 
 def test_valor_pagamento3():
