d17d626aa96adfd9b39d5b376407603891da0978
Vitmyself <vitor.duarte@aluno.faculdadeimpacta.com.br>
2019-10-03 21:13:23 -0300

    arrumando syntax

--- com/ac5/test/test_calcula_parcela.py
+++ com/ac5/test/test_calcula_parcela.py
@@ -2,13 +2,13 @@ import pytest
 from com.ac5.calcula_parcela import valorPagamento
 
 
-def test_valorPagamento1():
+def test_valor_pagamento1():
     assert valorPagamento(-1, 20) is None, "Should be None"
 
 
-def test_valorPagamento2():
+def test_valor_pagamento2():
     assert valorPagamento(80, 10) == 90.4, "Should be 90,4"
 
 
-def test_valorPagamento3():
+def test_valor_pagamento3():
     assert valorPagamento(120, 0) == 120, "Should be 120"
